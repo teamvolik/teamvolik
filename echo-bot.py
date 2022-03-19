d7dea198bed71f865cbde2bd40e0791fda977e40
Vladislav Rubtsov <dg.reflex@yandex.ru>
2022-03-19 22:52:46 +0300

    Create echo bot (#3) ### What's done:  * Added .gitignore to ignore config.json containing token for telegram bot  * Prepared a template for productive work on the echo bot  * Added logging to collect bot operation logs  * Added basic commands for the bot

--- echo-bot.py
+++ echo-bot.py
@@ -1,18 +1,28 @@
-#!/usr/bin/env python3.11
+#!/usr/bin/env python3.8
 
 import logging
 import json
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
-    pass
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True)
+    )
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    pass
+    update.message.reply_text("This is an echo bot that duplicates all messages sent to it.\n\
+                               To start working with the bot, type /start")
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Echo the user message."""
