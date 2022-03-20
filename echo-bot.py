498f812c420be2dd88d87a9c1a7f1cbcac1fe474
Vladislav Rubtsov <58705410+dgreflex@users.noreply.github.com>
2022-03-20 20:57:09 +0300

    Create echo bot (#5)
    Create echo bot (#3)
    ### What's done:
     * Added .gitignore to ignore config.json containing token for telegram bot
     * Prepared a template for productive work on the echo bot
     * Added logging to collect bot operation logs
     * Added basic commands for the bot
     * Implemented echo function for resending messages
     * Added the main function that implements the main work of the telegram bot through the token
    
    Co-authored-by: Alexander Frolov <lxnfrolov@gmail.com>

---
+++ echo-bot.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3.10
+# noqa D100
+import logging
+import json
+from telegram import Update, ForceReply
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    CallbackContext,
+    Filters,
+)
+
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    update.message.reply_markdown_v2(rf"Hi {user.mention_markdown_v2()}\!", reply_markup=ForceReply(selective=True))
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text("This is an echo bot that duplicates all messages sent to it.\nTo start working with the bot, type /start")
+
+
+def echo(update: Update, context: CallbackContext) -> None:
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def main() -> None:
+    """Start the bot."""
+    config_path = "config.json"
+    with open(config_path, "r") as config_file:
+        config = json.loads(config_file.read())
+
+    # Create the Updater to connect program with tg bot via token
+    updater = Updater(config["token"])
+
+    # Get the dispatcher to register handlers on it
+    dispatcher = updater.dispatcher
+
+    # Binding commands to the dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # Binding echo function to the dispatcher to echo the message on Telegram
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
