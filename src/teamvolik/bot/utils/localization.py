757c9cf2f8799d9ebd9db33993c0726c9536779c
Vladislav Rubtsov <dg.reflex@yandex.ru>
2022-06-16 17:46:24 +0300

    Wheel improvement #56 #What's done:  * Added wheel deps  * Removed dependency runtime installation  * Moved database to ~/.teamvolik directory  * Replacing a relative path with an absolute path indicating the translation of the corresponding localization

--- src/teamvolik/bot/utils/localization.py
+++ src/teamvolik/bot/utils/localization.py
@@ -1,6 +1,7 @@
 """Module imported functionality required for correct localization."""
 
 import gettext
+import os
 
-translation = gettext.translation("bot", "teamvolik/localization", fallback=True)
+translation = gettext.translation("bot", os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "localization"), fallback=True)
 _, ngettext = translation.gettext, translation.ngettext
